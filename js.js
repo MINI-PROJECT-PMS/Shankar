81de3dd06ff92379166de55cdab160aadfde08a5
Shankar-2002 <cecsshankars24@gmail.com>
2023-04-04 16:47:33 +0530

    04.04.2023

--- js.js
+++ js.js
@@ -1,8 +1,28 @@
-'use strict'
-document.querySelector('.dept').addEventListener('mouseenter',function(){
-    document.querySelector('.three').classList.remove('hidden');
-})
-document.querySelector('.dept').addEventListener('mouseleave',function()
+"use strict";
+const nav=document.querySelector('.select');
+const login=document.querySelector('.stud');
+const falogin=document.querySelector('.faculty');
+const login1=document.querySelector('.login');
+const login2=document.querySelector('.falogin');
+nav.addEventListener('click',function(e)
 {
-    document.querySelector('.three').classList.add('hidden');    
+    console.log(e.target);
+    
+    if(e.target.classList.contains('stud'))
+    {
+        login2.classList.add('hidden');
+     login1.classList.remove('hidden');
+     console.log("h")
+    }
+    if(e.target.classList.contains('faculty'))
+    {
+        login1.classList.add('hidden');
+     login2.classList.remove('hidden');
+    }
+})
+const body =document.querySelector('.body');
+body.addEventListener('click',function(e){
+    if(e.target.classList.contains('hi')) return;
+    login2.classList.add('hidden');
+    login1.classList.add('hidden');
 })
